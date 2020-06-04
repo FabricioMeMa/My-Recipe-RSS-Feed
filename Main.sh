a6ebef7469b2f898c1788012562be1fe1ab7dca3
Fabrício Marcelino <fabriciomesalira@gmail.com>
2020-06-04 03:31:15 +0000

    Script para download e envio
    Arquivo para download e envio do rss feed para o kindle

---
+++ Main.sh
@@ -0,0 +1,21 @@
+HOJE="$(date +%a_%d-%b-%Y)"
+PS="$(cat ~/Documents/News/Script/ps.txt)"
+EM="$(cat ~/Documents/News/Script/em.txt)"
+EK="$(cat ~/Documents/News/Script/ek.txt)"
+
+ebook-convert ~/Documents/News/Script/Git/Minhas\ Notícias.recipe ~/Documents/News/Minhas\ Notícias-$HOJE.mobi --output-profile kindle
+#notify-send  -t 10000 -i ~/Documentos/News/Script/jornal.png -a 🗞\ Minhas\ Notícias 'Notícias baixadas'
+cp ~/Documents/News/Minhas\ Notícias-$HOJE.mobi ~/Biblioteca\ do\ calibre/News/Minhas\ Notícias-$HOJE.mobi
+swaks --to $EK -s smtp.gmail.com:587 -tls -au $EM -ap $PS  --header "Noticias" --attach  ~/Documents/News/Minhas\ Notícias-$HOJE.mobi
+#echo "Minhas notícias de $HOJE" | mailx -r "FabMeMak1ndl3@kindle.com" -a ~/Documents/News/Minhas\ Notícias-$HOJE.mobi -s "Notícias $HOJE" -S smtp="smtp.gmail.com:587" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="fabriciomesalira@gmail.com" -S smtp-auth-password="mdcetcsagjvlmzee" -S ssl-verify=ignore FabMeMak1ndl3@kindle.com 
+#notify-send  -t 10000 -i ~/Documentos/News/Script/jornal.png -a 🗞\ Minhas\ Notícias 'Notícias enviadas para o seu Kindle'
+
+
+#Linha de comando Fish
+#set HOJE (date +%a_%d-%b-%Y) && ebook-convert ~/Documentos/News/Script/Minhas\ Notícias.recipe ~/Documentos/News/Minhas\ Notícias-$HOJE.mobi && echo "Minhas notícias de $HOJE" | mailx -v -r "FabMeMak1ndl3@kindle.com" -a ~/Documentos/News/🗞\ Minhas\ Notícias-$HOJE.mobi -s "Notícias $HOJE" -S smtp="smtp.gmail.com:587" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="fabriciomesalira@gmail.com" -S smtp-auth-password="mdcetcsagjvlmzee" -S ssl-verify=ignore FabMeMak1ndl3@kindle.com 
+
+
+#set HOJE (date +%a_%d-%b-%Y) && ebook-convert ~/Documentos/News/🗞\ Minhas\ Notícias-$HOJE.mobi ~/Documentos/News/🗞\ Minhas\ Notícias-$HOJE.docx
+#h  ~/Documents/News/Minhas\ Notícias-sáb_23-mai-2020.mobi
+
+#ebook-convert ~/Documents/News/Script/Minhas\ Notícias.recipe ~/Documents/News/Minhas.mobi --output-profile kindle --test --debug-pipeline debug
