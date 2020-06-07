10e6cba0490a440da5d3636b2bb935315b840459
Fabrício Marcelino <fabriciomesalira@gmail.com>
2020-06-07 14:38:57 +0000

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -5,7 +5,7 @@ EK="$(cat ~/Documents/News/Script/ek.txt)"
 
 ebook-convert ~/Documents/News/Script/Git/Minhas\ Notícias.recipe ~/Documents/News/Minhas\ Notícias-$HOJE.mobi --output-profile kindle
 #notify-send  -t 10000 -i ~/Documentos/News/Script/jornal.png -a 🗞\ Minhas\ Notícias 'Notícias baixadas'
-cp ~/Documents/News/Minhas\ Notícias-$HOJE.mobi ~/Biblioteca\ do\ calibre/News/Minhas\ Notícias-$HOJE.mobi
+#cp ~/Documents/News/Minhas\ Notícias-$HOJE.mobi ~/Biblioteca\ do\ calibre/News/Minhas\ Notícias-$HOJE.mobi
 swaks --to $EK -s smtp.gmail.com:587 -tls -au $EM -ap $PS  --header "Noticias" --attach  ~/Documents/News/Minhas\ Notícias-$HOJE.mobi
 #echo "Minhas notícias de $HOJE" | mailx -r "FabMeMak1ndl3@kindle.com" -a ~/Documents/News/Minhas\ Notícias-$HOJE.mobi -s "Notícias $HOJE" -S smtp="smtp.gmail.com:587" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="fabriciomesalira@gmail.com" -S smtp-auth-password="mdcetcsagjvlmzee" -S ssl-verify=ignore FabMeMak1ndl3@kindle.com 
 #notify-send  -t 10000 -i ~/Documentos/News/Script/jornal.png -a 🗞\ Minhas\ Notícias 'Notícias enviadas para o seu Kindle'
