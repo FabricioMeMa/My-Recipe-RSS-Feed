1b9c30396f4c575cb8b31948e1a675754115f70b
Fabrício Marcelino <fabriciomesalira@gmail.com>
2022-06-25 12:56:18 -0300

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -3,19 +3,19 @@ PS="$(cat ~/Documents/News/Script/ps.txt)"
 EM="$(cat ~/Documents/News/Script/em.txt)"
 EK="$(cat ~/Documents/News/Script/ek.txt)"
 
-ebook-convert ~/Documents/News/Script/Git/Minhas\ Notícias.recipe ~/Documents/News/Jornal/Minhas\ Notícias-$HOJE.mobi --output-profile kindle
+ebook-convert ~/Documents/News/Script/Git/Minhas\ Notícias.recipe ~/Documents/News/Jornal/Minhas\ Notícias-$HOJE.epub --output-profile kindle
 #notify-send  -t 10000 -i ~/Documentos/News/Script/jornal.png -a 🗞\ Minhas\ Notícias 'Notícias baixadas'
-#cp ~/Documents/News/Minhas\ Notícias-$HOJE.mobi ~/Biblioteca\ do\ calibre/News/Minhas\ Notícias-$HOJE.mobi
+#cp ~/Documents/News/Minhas\ Notícias-$HOJE.mobi ~/Biblioteca\ do\ calibre/News/Minhas\ Notícias-$HOJE.epub
 swaks --to $EK -s smtp.gmail.com:587 -tls -au $EM -ap $PS  --header "Subject: Convert" --attach  ~/Documents/News/Jornal/Minhas\ Notícias-$HOJE.epub
-#echo "Minhas notícias de $HOJE" | mailx -r "FabMeMak1ndl3@kindle.com" -a ~/Documents/News/Minhas\ Notícias-$HOJE.mobi -s "Notícias $HOJE" -S smtp="smtp.gmail.com:587" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="email@email.com" -S smtp-auth-password="password" -S ssl-verify=ignore sendto@email.com
+#echo "Minhas notícias de $HOJE" | mailx -r "FabMeMak1ndl3@kindle.com" -a ~/Documents/News/Minhas\ Notícias-$HOJE.epub -s "Notícias $HOJE" -S smtp="smtp.gmail.com:587" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="email@email.com" -S smtp-auth-password="password" -S ssl-verify=ignore sendto@email.com
 #notify-send  -t 10000 -i ~/Documentos/News/Script/jornal.png -a 🗞\ Minhas\ Notícias 'Notícias enviadas para o seu Kindle'
 
 
 #Linha de comando Fish
-#set HOJE (date +%a_%d-%b-%Y) && ebook-convert ~/Documentos/News/Script/Minhas\ Notícias.recipe ~/Documentos/News/Minhas\ Notícias-$HOJE.mobi && echo "Minhas notícias de $HOJE" | mailx -v -r "FabMeMak1ndl3@kindle.com" -a ~/Documentos/News/🗞\ Minhas\ Notícias-$HOJE.mobi -s "Notícias $HOJE" -S smtp="smtp.gmail.com:587" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="fabriciomesalira@gmail.com" -S smtp-auth-password="mdcetcsagjvlmzee" -S ssl-verify=ignore FabMeMak1ndl3@kindle.com 
+#set HOJE (date +%a_%d-%b-%Y) && ebook-convert ~/Documentos/News/Script/Minhas\ Notícias.recipe ~/Documentos/News/Minhas\ Notícias-$HOJE.epub && echo "Minhas notícias de $HOJE" | mailx -v -r "FabMeMak1ndl3@kindle.com" -a ~/Documentos/News/🗞\ Minhas\ Notícias-$HOJE.mobi -s "Notícias $HOJE" -S smtp="smtp.gmail.com:587" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="fabriciomesalira@gmail.com" -S smtp-auth-password="mdcetcsagjvlmzee" -S ssl-verify=ignore FabMeMak1ndl3@kindle.com 
 
 
-#set HOJE (date +%a_%d-%b-%Y) && ebook-convert ~/Documentos/News/🗞\ Minhas\ Notícias-$HOJE.mobi ~/Documentos/News/🗞\ Minhas\ Notícias-$HOJE.docx
+#set HOJE (date +%a_%d-%b-%Y) && ebook-convert ~/Documentos/News/🗞\ Minhas\ Notícias-$HOJE.epub ~/Documentos/News/🗞\ Minhas\ Notícias-$HOJE.docx
 #h  ~/Documents/News/Minhas\ Notícias-sáb_23-mai-2020.mobi
 
 #ebook-convert ~/Documents/News/Script/Minhas\ Notícias.recipe ~/Documents/News/Minhas.mobi --output-profile kindle --test --debug-pipeline debug
