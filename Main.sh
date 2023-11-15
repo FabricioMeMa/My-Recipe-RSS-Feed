14c6356448c7a7a09e0d8dc1a00485e772802765
Fabrício Marcelino <fabriciomesalira@gmail.com>
2023-11-15 11:01:00 -0300

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -5,14 +5,15 @@ EK="$(cat ~/Documents/News/Script/ek.txt)"
 EKL="$(cat ~/Documents/News/Script/ekl.txt)"
 
 ebook-convert ~/Documents/News/Script/Git/Minhas\ Notícias.recipe ~/Documents/News/Jornal/Minhas\ Noticias-$HOJE.mobi --output-profile kindle
+calibredb add ~/Documents/News/Jornal/Minhas\ Noticias-$HOJE.mobi
 #ebook-convert ~/Documents/News/Jornal/Minhas\ Notícias-$HOJE.mobi ~/Área\ de\ trabalho/Minhas\ Notícias-$HOJE.epub
 #notify-send  -t 10000 -i ~/Documentos/News/Script/jornal.png -a 🗞\ Minhas\ Notícias 'Notícias baixadas'
 #cp ~/Documents/News/Minhas\ Notícias-$HOJE.mobi ~/Biblioteca\ do\ calibre/News/Minhas\ Notícias-$HOJE.epub
-swaks --to $EK -s smtp.gmail.com:587 -tls -au $EM -ap $PS  --header "Subject: " --attach  ~/Documents/News/Jornal/Minhas\ Noticias-$HOJE.mobi
+#swaks --to $EK -s smtp.gmail.com:587 -tls -au $EM -ap $PS  --header "Subject: " --attach  ~/Documents/News/Jornal/Minhas\ Noticias-$HOJE.mobi
 #echo "Minhas notícias de $HOJE" | mailx -r "FabMeMak1ndl3@kindle.com" -a ~/Documents/News/Minhas\ Notícias-$HOJE.epub -s "Notícias $HOJE" -S smtp="smtp.gmail.com:587" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="email@email.com" -S smtp-auth-password="password" -S ssl-verify=ignore sendto@email.com
 #notify-send  -t 10000 -i ~/Documentos/News/Script/jornal.png -a 🗞\ Minhas\ Notícias 'Notícias enviadas para o seu Kindle'
-ebook-convert ~/Documents/News/Script/Git/NoticiasLe.recipe ~/Documents/News/Jornal/Leandro/Noticias-$HOJE.mobi --output-profile kindle
-swaks --to $EKL -s smtp.gmail.com:587 -tls -au $EM -ap $PS  --header "Subject: " --attach  ~/Documents/News/Jornal/Leandro/Noticias-$HOJE.mobi
+#ebook-convert ~/Documents/News/Script/Git/NoticiasLe.recipe ~/Documents/News/Jornal/Leandro/Noticias-$HOJE.mobi --output-profile kindle
+#swaks --to $EKL -s smtp.gmail.com:587 -tls -au $EM -ap $PS  --header "Subject: " --attach  ~/Documents/News/Jornal/Leandro/Noticias-$HOJE.mobi
 
 
 #Linha de comando Fish
